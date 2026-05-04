8e2299410d51764c55a6a9d299480b85f95d79bd
Vishakha Gautam <156678713+Vishakha2431@users.noreply.github.com>
2026-05-04 20:52:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-//your JS code here. If required.
+let res = document.getElementById("output");
+
+document.addEventListener("DOMContentLoaded", () => {
+    let tr = document.createElement("tr");
+    tr.innerHTML = `<td colspan="2" style="text-align:center;">Loading...</td>`;
+    res.appendChild(tr);
+
+    function createPromise() {
+        let time = Math.random();
+
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(time);
+            }, time * 1000);
+        });
+    }
+
+    Promise.all([createPromise(), createPromise(), createPromise()])
+    .then((values) => {
+        res.innerHTML = "";
+
+        values.forEach((time, index) => {
+            let tr = document.createElement("tr");
+            tr.innerHTML = `
+                <td>Promise ${index + 1}</td>
+                <td>${time.toFixed(3)}</td>
+            `;
+            res.appendChild(tr);
+        });
+
+        let total = Math.max(...values);
+
+        let tr = document.createElement("tr");
+        tr.innerHTML = `
+            <td>Total</td>
+            <td>${total.toFixed(3)}</td>
+        `;
+        res.appendChild(tr);
+    });
+});
